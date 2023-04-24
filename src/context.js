76b9083034c6b696d26a4eeaf8bc1a4d08e6b2a7
Hrodberht <hrodberht.stefanescu@gmail.com>
2023-04-24 09:35:53 +0100

    Fetch questions

--- src/context.js
+++ src/context.js
@@ -26,6 +26,29 @@ const AppProvider = ({ children }) => {
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const fetchQuestions = async (url) => {
+    setLoading(true);
+    setWaiting(false);
+    const response = await axios(url).catch((err) => console.log(err));
+
+    if (response) {
+      const data = response.data.results;
+      if (data.length > 0) {
+        setQuestions(data);
+        setLoading(false);
+        setWaiting(false);
+        setError(false);
+      }
+    } else {
+      setWaiting(true);
+      setError(true);
+    }
+  };
+
+  useEffect(() => {
+    fetchQuestions(tempUrl);
+  }, []);
+
   return (
     <AppContext.Provider
       value={{
